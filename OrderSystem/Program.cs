7b3bdde4dd842eae50b0f2bf9d1d84b07179fe76
Van Eester Michiel <michielvaneester@gmail.com>
2022-05-30 11:04:21 +0200

    Rename Main form
    Form1 -> FormMDI

--- OrderSystem/Program.cs
+++ OrderSystem/Program.cs
@@ -16,7 +16,7 @@ namespace OrderSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormMDI());
         }
     }
 }
