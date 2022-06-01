a5854fd3c8c460aac34e6e6025909c923012897b
Van Eester Michiel <michielvaneester@gmail.com>
2022-06-01 09:38:36 +0200

    Update DBControl.cs

--- DBControl/DBControl.cs
+++ DBControl/DBControl.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,87 @@ namespace DBControl
 {
     public class DBControl
     {
+        public static string mCS = "";
 
+        public OleDbConnection OpenConnection()
+        {
+            OleDbConnection cnn = new OleDbConnection(mCS);
+
+            try
+            {
+                cnn.Open();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return cnn;
+        }
+
+        public bool CloseConnection(OleDbConnection cnn)
+        {
+            try
+            {
+                cnn.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return true;
+        }
+
+        public static DataTable GetDataTable(string sql)
+        {
+            OleDbConnection cnn = new OleDbConnection(mCS);
+            OleDbCommand cmd = new OleDbCommand(sql, cnn);
+            DataTable dt = new DataTable();
+
+            try
+            {
+                cnn.Open();
+                OleDbDataAdapter oda = new OleDbDataAdapter(cmd);
+                oda.Fill(dt);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                cnn.Close();
+            }
+            return dt;
+        }
+
+        public bool UpdateDatabase(string sql)
+        {
+            OleDbConnection cnn = new OleDbConnection(mCS);
+            OleDbCommand cmd = new OleDbCommand(sql, cnn);
+            int result;
+
+            try
+            {
+                cnn.Open();
+                result = cmd.ExecuteNonQuery();
+
+                if (result < 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                cnn.Close();
+            }
+        }
     }
 }
