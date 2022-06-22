059224e97766e051828a11935f66846f504b7aea
Van Eester Michiel <michielvaneester@gmail.com>
2022-06-22 10:30:14 +0200

    Manage - Orders
    Load asst,
    front end

--- Order/FormMakeOrder.Designer.cs
+++ Order/FormMakeOrder.Designer.cs
@@ -44,19 +44,248 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.cboCategory = new System.Windows.Forms.ComboBox();
+            this.dgvAssortiment = new System.Windows.Forms.DataGridView();
+            this.dgvOrder = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.pnlFilterAsst = new System.Windows.Forms.Panel();
+            this.lblOrder = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.ArticleNr = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ArticleDesc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Category = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SubCategory = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAssortiment)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrder)).BeginInit();
+            this.pnlFilterAsst.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Location = new System.Drawing.Point(3, 12);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(52, 13);
+            this.lblCategory.TabIndex = 0;
+            this.lblCategory.Text = "Category:";
+            // 
+            // cboCategory
+            // 
+            this.cboCategory.FormattingEnabled = true;
+            this.cboCategory.Location = new System.Drawing.Point(61, 9);
+            this.cboCategory.Name = "cboCategory";
+            this.cboCategory.Size = new System.Drawing.Size(155, 21);
+            this.cboCategory.TabIndex = 1;
+            this.cboCategory.TextChanged += new System.EventHandler(this.cboCategory_TextChanged);
+            // 
+            // dgvAssortiment
+            // 
+            this.dgvAssortiment.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAssortiment.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ArticleNr,
+            this.ArticleDesc,
+            this.Category,
+            this.SubCategory,
+            this.Price});
+            this.dgvAssortiment.Location = new System.Drawing.Point(15, 74);
+            this.dgvAssortiment.Name = "dgvAssortiment";
+            this.dgvAssortiment.Size = new System.Drawing.Size(625, 601);
+            this.dgvAssortiment.TabIndex = 2;
+            // 
+            // dgvOrder
+            // 
+            this.dgvOrder.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOrder.Location = new System.Drawing.Point(756, 74);
+            this.dgvOrder.Name = "dgvOrder";
+            this.dgvOrder.Size = new System.Drawing.Size(625, 601);
+            this.dgvOrder.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(646, 178);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(104, 45);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = ">";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemove.Location = new System.Drawing.Point(646, 246);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(104, 45);
+            this.btnRemove.TabIndex = 5;
+            this.btnRemove.Text = "<";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(1306, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 6;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(1306, 681);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(3, 39);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(44, 13);
+            this.lblSearch.TabIndex = 8;
+            this.lblSearch.Text = "Search:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(61, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(155, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(222, 35);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 10;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // pnlFilterAsst
+            // 
+            this.pnlFilterAsst.Controls.Add(this.lblCategory);
+            this.pnlFilterAsst.Controls.Add(this.btnRefresh);
+            this.pnlFilterAsst.Controls.Add(this.cboCategory);
+            this.pnlFilterAsst.Controls.Add(this.textBox1);
+            this.pnlFilterAsst.Controls.Add(this.lblSearch);
+            this.pnlFilterAsst.Location = new System.Drawing.Point(12, 5);
+            this.pnlFilterAsst.Name = "pnlFilterAsst";
+            this.pnlFilterAsst.Size = new System.Drawing.Size(301, 63);
+            this.pnlFilterAsst.TabIndex = 11;
+            // 
+            // lblOrder
+            // 
+            this.lblOrder.AutoSize = true;
+            this.lblOrder.Location = new System.Drawing.Point(778, 44);
+            this.lblOrder.Name = "lblOrder";
+            this.lblOrder.Size = new System.Drawing.Size(36, 13);
+            this.lblOrder.TabIndex = 12;
+            this.lblOrder.Text = "Order:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(820, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(143, 20);
+            this.textBox2.TabIndex = 13;
+            // 
+            // ArticleNr
+            // 
+            this.ArticleNr.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ArticleNr.DataPropertyName = "asstArticleNumber";
+            this.ArticleNr.HeaderText = "Article Number";
+            this.ArticleNr.Name = "ArticleNr";
+            // 
+            // ArticleDesc
+            // 
+            this.ArticleDesc.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ArticleDesc.DataPropertyName = "asstArticleDescription";
+            this.ArticleDesc.HeaderText = "Article Description";
+            this.ArticleDesc.Name = "ArticleDesc";
+            // 
+            // Category
+            // 
+            this.Category.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Category.DataPropertyName = "asstCategory";
+            this.Category.HeaderText = "Category";
+            this.Category.Name = "Category";
+            // 
+            // SubCategory
+            // 
+            this.SubCategory.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.SubCategory.DataPropertyName = "asstSubCategory";
+            this.SubCategory.HeaderText = "Sub Category";
+            this.SubCategory.Name = "SubCategory";
+            // 
+            // Price
+            // 
+            this.Price.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Price.DataPropertyName = "asstPrice";
+            this.Price.HeaderText = "Price";
+            this.Price.Name = "Price";
             // 
             // FormMakeOrder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1393, 716);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblOrder);
+            this.Controls.Add(this.pnlFilterAsst);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dgvOrder);
+            this.Controls.Add(this.dgvAssortiment);
             this.Name = "FormMakeOrder";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormMakeOrder";
+            this.Load += new System.EventHandler(this.FormMakeOrder_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAssortiment)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrder)).EndInit();
+            this.pnlFilterAsst.ResumeLayout(false);
+            this.pnlFilterAsst.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.ComboBox cboCategory;
+        private System.Windows.Forms.DataGridView dgvAssortiment;
+        private System.Windows.Forms.DataGridView dgvOrder;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Panel pnlFilterAsst;
+        private System.Windows.Forms.Label lblOrder;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ArticleNr;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ArticleDesc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Category;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SubCategory;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
     }
 }
--- Order/FormMakeOrder.cs
+++ Order/FormMakeOrder.cs
@@ -7,19 +7,73 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DBControl;
 
 namespace Order
 {
     public partial class FormMakeOrder : Form
     {
         #region Variables
+        DBOrderSystem dbo = new DBOrderSystem();
+        DataTable dtAsst = new DataTable();
+        DataTable dtOrder = new DataTable();    
+
         bool edit = false;
+        bool load = true;
+
         #endregion
 
         #region Form
         public FormMakeOrder()
         {
             InitializeComponent();
+        }
+
+        private void FormMakeOrder_Load(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt = dbo.GetCategories();
+            cboCategory.DataSource = dt;
+            this.cboCategory.DisplayMember = "spCategory";
+
+            GetGridAsst();
+            load = false;
+        }
+        #endregion
+
+        #region Buttons
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboCategory_TextChanged(object sender, EventArgs e)
+        {
+            if (!load)
+            {
+                GetGridAsst();
+            }
         }
         #endregion
 
@@ -29,6 +83,31 @@ namespace Order
 
 
             edit = true;
+        }
+
+        public void GetGridAsst()
+        {
+            string cat = "";
+            string subCat = "";
+
+            if (load)
+            {
+                cat = "-- Default --";
+            }
+            else
+            {
+                cat = cboCategory.Text;
+            }
+
+            try
+            {
+                dtAsst = dbo.GetArticle(cat, subCat);
+                dgvAssortiment.DataSource = dtAsst;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
         #endregion
     }
